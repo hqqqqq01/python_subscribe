5cbee809f5b8820012072001c22aca0712f69cbf
hqqqqq01 <60255102+hqqqqq01@users.noreply.github.com>
2023-07-05 17:05:15 +0800

    Create focus_clock.py

---
+++ focus_clock.py
@@ -0,0 +1,20 @@
+import time
+
+def focus_timer(minutes):
+    seconds = minutes * 60
+    end_time = time.time() + seconds
+
+    print(f"专注倒计时开始，将持续 {minutes} 分钟。")
+
+    while time.time() < end_time:
+        remaining_seconds = int(end_time - time.time())
+        minutes = remaining_seconds // 60
+        seconds = remaining_seconds % 60
+        time_format = f"{minutes:02d}:{seconds:02d}"
+        print(f"剩余时间：{time_format}", end="\r")
+        time.sleep(1)
+
+    print("\n专注时间结束！")
+
+# 设置专注时间为25分钟
+focus_timer(25)
